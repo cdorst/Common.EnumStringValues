7c2dadc05ff985634c6e5ad819607ee67b583179
Christopher Dorst <chris@dorst.email>
2017-12-04 14:59:39 -0600

    Adding extensions and attributes

---
+++ Common.EnumStringValues/EnumExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Common.EnumStringValues
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns the first instance of attribute
+        /// <typeparamref name="T"/> for the given enum value
+        /// </summary>
+        public static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            var type = value.GetType();
+            var memberInfo = type.GetMember(value.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+            return (T)attributes[0];
+        }
+
+        /// <summary>
+        /// Returns description metadata for the given enum value
+        /// </summary>
+        public static string GetDescription(this Enum value)
+        {
+            var attribute = value.GetAttribute<EnumStringValueAttribute>();
+            return attribute == null ? value.ToString() : attribute.Value;
+        }
+
+        /// <summary>
+        /// Returns the enum value matching the given EnumStringValue attribute value
+        /// </summary>
+        public static T GetEnumValue<T>(this string value)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(EnumStringValueAttribute)) is EnumStringValueAttribute attribute)
+                    if (attribute.Value == value) return (T)field.GetValue(null);
+                else
+                    if (field.Name == value) return (T)field.GetValue(null);
+            }
+            return default(T);
+        }
+    }
+}
---
+++ Common.EnumStringValues/EnumStringValueAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common.EnumStringValues
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public sealed class EnumStringValueAttribute : Attribute
+    {
+        private readonly string _value;
+
+        public EnumStringValueAttribute(string value)
+        {
+            _value = value;
+        }
+
+        public string Value => _value;
+    }
+}
